29f58226baa35308d2f028e213cf80c70485cdd7
Mike Turley <mike@miketurley.com>
2013-11-26 21:12:12 -0500

    really simple routing for logged in / active sequence state.

--- annoying.js
+++ annoying.js
@@ -1,20 +1,52 @@
 // TODO put collections here (equiv to tables)
 
+Data = {
+  Sequences : new Meteor.Collection("Sequences"),
+  newSequence : function() {
+    return {
+      title : "Untitled"
+    };
+  },
+  getActiveSequence : function() {
+    return Data.Sequences.findOne(State.getActiveSequenceId());
+  }
+};
+
+State = {
+  loggedIn : function() {
+    return Meteor.user() != null;
+  },
+  setActiveSequenceId : function(sequenceId) {
+    if(!State.loggedIn()) {
+      console.warn("Can't set active sequence id, not logged in.");
+      return;
+    }
+    Meteor.users.update({_id:Meteor.user()._id}, {$set:{"profile.activeSequenceId":sequenceId}})
+  },
+  getActiveSequenceId : function() {
+    if(State.loggedIn()) return Meteor.user().profile.activeSequenceId;
+    console.warn("Can't get active sequence id, not logged in.");
+    return null;
+  }
+};
+
 if (Meteor.isClient) {
 
   Template.body.githubURL = "http://github.com/mturley/annoying";
-
-  Template.hello.greeting = function () {
-    return "Welcome to annoying.";
+  Template.body.loggedIn = State.loggedIn;
+  Template.body.hasActiveSequence = function() {
+    return State.getActiveSequenceId() != null;
   };
+  Template.body.activeSequenceId = State.getActiveSequenceId;
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+  Template.body.events({
+    'click .btn.create-sequence' : function() {
+      console.log("Clicked!");
+      var sequenceId = Data.Sequences.insert(Data.NewSequence());
+      State.setActiveSequenceId(sequenceId);
     }
-  });
+  })
+
 }
 
 if (Meteor.isServer) {
